e10629a2a5556d2451f4078cb86b78ef869dd8f0
necojackarc <necojackarc@gmail.com>
2015-06-25 20:15:27 +0900

    Add memo to RotetaLines

--- golf/RotateLines.hs
+++ golf/RotateLines.hs
@@ -1,3 +1,11 @@
 -- Under construction...
 f (a:b) = b ++ [a]
 main = interact $ unlines.f.lines
+
+n = do
+  first <- getLine
+  interact (\x -> x ++ first)
+
+main = do
+  first <- getLine
+  (interact . flip (++)) first
