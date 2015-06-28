e0a9e9c3df7126ce45f8c30c793909e8e84d5dc2
necojackarc <necojackarc@gmail.com>
2015-06-29 03:07:13 +0900

    Golf gcd

--- golf/GreatestCommonDivisor.hs
+++ golf/GreatestCommonDivisor.hs
@@ -1,6 +1,13 @@
 -- まずはシンプルに
-getInt = fmap (\x -> read x :: Int) getLine
-main = do
-  a <- getInt
-  b <- getInt
-  print $ gcd a b
+-- main = do
+--   a <- readLn
+--   b <- readLn
+--   print $ gcd a b
+
+-- そのままゴルフ
+-- r=readLn
+-- main=do a<-r;b<-r;print$gcd a b
+
+r = readLn
+main = r >>= (\x -> r >>= (\y -> print $ gcd x y))
+
